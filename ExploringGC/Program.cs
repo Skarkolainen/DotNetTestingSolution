4a941651ead1a11013e6d261d765ecec975837d8
Simen Skarkerud <simen.skarkerud@winorg.no>
2024-10-29 13:45:21 +0100

    Exploring Garbage collector

--- ExploringGC/Program.cs
+++ ExploringGC/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+
+//Creates object in Gen 0
+
+MyClass obj1 = new();
+
+DoWork();
+
+GC.Collect(0);
+Console.WriteLine("GC gen 0 collected");
+
+
+MyClass obj2 = new();
+
+
+static void DoWork()
+{
+    for (int i = 0; i < 100; i++)
+    {
+        MyClass tempbObj = new();
+    }
+}
+class MyClass
+{
+    public int MyProperty { get; set; }
+}
